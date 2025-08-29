8697dee1463be458da47f834b633ea7fc6e8ac5a
RunRunDog-Simon <g4ru4j32l4@gmail.com>
2025-08-29 19:41:30 +0800

    initialize projects

---
+++ src/main/java/com/gtelant/commerce_admin_service/CommerceAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gtelant.commerce_admin_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceAdminServiceApplication.class, args);
+	}
+
+}

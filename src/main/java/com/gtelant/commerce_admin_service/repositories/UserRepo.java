2ca265e6f874fb69ce6b0eb9294d0c887c38a936
RunRunDog-Simon <g4ru4j32l4@gmail.com>
2025-09-15 21:13:43 +0800

    UserRepo Update

--- src/main/java/com/gtelant/commerce_admin_service/repositories/UserRepo.java
+++ src/main/java/com/gtelant/commerce_admin_service/repositories/UserRepo.java
@@ -3,27 +3,21 @@ package com.gtelant.commerce_admin_service.repositories;
 import com.gtelant.commerce_admin_service.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
 import java.util.List;
+import java.util.Optional;
 
 
-public interface UserRepo extends JpaRepository<User, Long>, JpaSpecificationExecutor {
-//    Page <User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
-//            String firstName, String lastName, PageRequest pageRequest
-//    );
-//
-//    Page<User> findByHasNewsletterTrue(Pageable pageable);
-//
-//
+public interface UserRepo extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    //以下可以被Specification取代
+    Page<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            String firstName, String lastName, PageRequest pageRequest
+    );
+    List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            String firstName, String lastName);
 
-//    Optional<User> findByLastNameAndFirstName(String lastName, String firstName);
-//    以上這行ChatGPT提供以下解法
-//    @Query("select u from User u where u.lastName = :lastName and u.firstName = :firstName")
-//    Optional<User> findByFullName(@Param("lastName") String ln,
-//                                  @Param("firstName") String fn);
-//    Optional<User> findByEmail(String email);
+    // 以下新增EMAIL查詢方法
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
